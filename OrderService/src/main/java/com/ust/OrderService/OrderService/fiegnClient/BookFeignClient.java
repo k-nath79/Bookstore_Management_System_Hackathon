35e6936c20432aff13dfeb8dd01e7cd2f94d6450
285537 <Kailasnath.S@ust.com>
2024-08-19 09:59:12 +0530

    Eureka Implemented

--- OrderService/src/main/java/com/ust/OrderService/OrderService/fiegnClient/BookFeignClient.java
+++ OrderService/src/main/java/com/ust/OrderService/OrderService/fiegnClient/BookFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "BookService",url = "http://localhost:8100")
+@FeignClient(name = "BookService")
 public interface BookFeignClient {
     @GetMapping("/api/book/getquantity/{id}")
     int getQuantity(@PathVariable long id);
